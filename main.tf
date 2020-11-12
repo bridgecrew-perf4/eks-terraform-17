0f0e4d997cbaaebd4571ed32cd6f473501549f34
감민재 <mjkam@adminui-MacBookPro.local>
2020-11-12 18:19:29 +0900

    완성

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ resource "aws_subnet" "public_subnet_1" {
 
   tags = {
     Name = "public_subnet_1"
+    "kubernetes.io/cluster/prod" = "shared"
   }
 }
 
@@ -39,6 +40,7 @@ resource "aws_subnet" "public_subnet_2" {
 
   tags = {
     Name = "public_subnet_2"
+    "kubernetes.io/cluster/prod" = "shared"
   }
 }
 
@@ -50,6 +52,7 @@ resource "aws_subnet" "public_subnet_3" {
 
   tags = {
     Name = "public_subnet_3"
+    "kubernetes.io/cluster/prod" = "shared"
   }
 }
 
@@ -175,12 +178,47 @@ resource "aws_eks_cluster" "prod" {
 #   ]
 # }
 
+resource "aws_iam_role" "worker_role" {
+  name = "worker_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach1" {
+  role       = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "attach2" {
+  role       = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "attach3" {
+  role       = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
 
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.prod.name
   node_group_name = "standard-workers"
-  node_role_arn   = aws_iam_role.example.arn
-  subnet_ids      = aws_subnet.example[*].id
+  node_role_arn   = aws_iam_role.worker_role.arn
+  subnet_ids      = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
+  instance_types = ["t3.medium"]
 
   scaling_config {
     desired_size = 1
@@ -191,8 +229,8 @@ resource "aws_eks_node_group" "example" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.attach1,
+    aws_iam_role_policy_attachment.attach2,
+    aws_iam_role_policy_attachment.attach3,
   ]
 }
